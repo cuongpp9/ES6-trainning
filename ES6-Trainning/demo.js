f9d136f0b57f09e40a3614fe94c341b97ace82c9
cuongpp9 <cuongpp9@gmail.com>
2018-08-17 16:58:39 +0700

    create app

---
+++ ES6-Trainning/demo.js
@@ -0,0 +1,50 @@
+const input = {
+    firstName: 'pham',
+    lastName: 'cuong',
+    a: 6,
+    b: 9,
+}
+
+////////////////////////////Default parameter///////////////////////////
+function defaultParameter(firstName, lastName, fullName = firstName + lastName){
+    lastName = lastName || 'abc';
+    return [firstName, lastName, fullName];
+}
+
+console.log(defaultParameter(input.firstName));
+console.log(defaultParameter(input.firstName, input.lastName));
+
+////////////////////////////TemplateLiterals///////////////////////////
+const {a,b} = input;
+console.log(`Fifteen is ${a+b}
+and fifty four is ${a*b}`);
+console.log(`a=b? ${a==b? 'true':'false'}`);
+
+function tag(strings) {
+    console.log(strings.raw[0]);
+  }
+tag `string text line 1 \n string text line 2`;
+
+////////////////////////////Multi-line///////////////////////////
+console.log(`first name = ${input.firstName}
+last name = ${input.lastName}
+=====>> full name = ${input.firstName + input.lastName}`);
+
+////////////////////////////Destructuring assignment///////////////////////////
+const [number1, number2, ...rest] = [10, 20, 30, 40, 50, 60, 70];
+console.log('Destructuring:', rest);
+
+///////////////////////Enhanced Object Literals///////////////////////
+//object Variables
+const n1 = 1, n2 = 2, n3 = 3;
+  obj = {
+    n1,
+    n2,
+    n3,
+};
+
+const lib = {
+    sum(a,b){return a+b; },
+    mult(a,b){return a*b;}
+}
+console.log(lib.sum(1,2));
